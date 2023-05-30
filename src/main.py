75d92d96cbc28395a8e7912a45829f31d620ae67
tanaya-k <tanayakaundal2021@gmail.com>
2023-05-30 00:40:53 -0700

    Created jsons for all spell classes

--- src/main.py
+++ src/main.py
@@ -1,2 +1,10 @@
-import spellsInfo
-import listOfSpellsInfo
+import json
+
+with open('spell-list.json') as spell_list:
+    data = json.load(spell_list)
+
+# Input format, classType is the varibale that can be used with print now
+# classType = input("Please select your class:")
+
+
+# Generate jsons for the different classes
