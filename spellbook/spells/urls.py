545c533074eb2764210039497e514a81c42c6762
tanaya-k <tanayakaundal2021@gmail.com>
2023-11-01 11:51:10 -0700

    Implemented basic template for spell display

--- spellbook/spells/urls.py
+++ spellbook/spells/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name='index')
+    path("", views.index, name='index'),
+    path("<int:spell_id>/", views.description, name="description"),
 ]
--- spellbook/spells/views.py
+++ spellbook/spells/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Spell
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hey brah you're at the spells index")
+    spell_list = Spell.objects.all()
+    output = ", ".join([s.name for s in spell_list])
+    return HttpResponse(output)
+
+def description(request, spell_id):
+    spell_desc = Spell.objects.get(pk = spell_id)
+    template = loader.get_template("spells/description.html")
+    context = {
+        "spell_desc": spell_desc,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+
+# maybe add spellbook account view
